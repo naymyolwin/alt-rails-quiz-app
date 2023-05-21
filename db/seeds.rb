9842d865a9fd66eaf1616d5148dd9ce9b4ca217f
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-21 15:36:10 +0700

    add a few more record using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Job.create(url: "www.netflix.com", employer_name: "Netflix", employer_description: "Netflix Co.ltd", job_title: "React Developer", job_description: "do some react code", year_of_experience: 5, education_requirement: "Batchelor Degree", industry: "Streaming", base_salary: 75000, employment_type_id: 3 )
+
+Job.create(url: "www.facebook.com", employer_name: "Facebook", employer_description: "Facebook Co.ltd", job_title: "React Developer", job_description: "do some react code", year_of_experience: 5, education_requirement: "Batchelor Degree", industry: "Social Media", base_salary: 70000, employment_type_id: 4 )
+
+Job.create(url: "www.youtube.com", employer_name: "Youtube", employer_description: "Youtube Co.ltd", job_title: "React Developer", job_description: "do some react code", year_of_experience: 5, education_requirement: "Batchelor Degree", industry: "Streaming", base_salary: 65000, employment_type_id: 4 )
