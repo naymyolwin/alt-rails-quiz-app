7b938d5e1affe56a8814fa0a96b3f8eb93f63c5f
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-21 16:45:26 +0700

    add create form and function

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -13,7 +13,18 @@ class JobsController < ApplicationController
     end
 
     def create
+        @job = Job.new(job_params)
+        if @job.save
+            redirect_to root_path
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
 
+    private
+
+    def job_params
+        params.require(:job).permit(:url, :employer_name, :employer_description, :job_title, :job_description, :year_of_experience, :education_requirement, :industry, :base_salary, :employment_type_id)
     end
 
 end
