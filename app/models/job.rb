7a874154d7f76eb85de04ce9838c3957b3933594
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-21 22:13:39 +0700

    add ui for index and show page

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,12 @@
 class Job < ApplicationRecord
+    validates :url, presence: true
+    validates :employer_name, presence: true
+    validates :employer_description, presence: true
+    validates :job_title, presence: true
+    validates :job_description, presence: true
+    validates :year_of_experience, presence: true
+    validates :education_requirement, presence: true
+    validates :industry, presence: true
+    validates :base_salary, presence: true
+    validates :employment_type_id, presence: true
 end
